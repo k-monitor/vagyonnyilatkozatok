4bacfae06f9f8c19adccf052ba37ba99faf404cc
ejulcsi <ejulcsi@gmail.com>
2016-01-31 17:08:06 +0100

    new intro page, iframes fixed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var url = 'json/data.json', // az adatlapokat tartalmazo json elerhetosege
+var url = 'json/data.json', // az adatlapokat tartalmazo json elerhetosege az index.html-hez kepest
     data;
 
 getPDF();
@@ -16,12 +16,14 @@ function clickForLinks() {
     $('button').click(function() {
         var item = getRandomItem(data)
             pdf_url = item.pdf_url,
+            person_url = item.person_url,
             name = item.name,
             google_url = 'https://docs.google.com/viewer?embedded=true&url=' + encodeURIComponent(pdf_url);
 
         $('body').addClass('small');
         $('#frames').addClass('show');
         $('#person').text(name);
+        $('#person').attr('href', person_url);
         $('#link').attr('src', google_url);
     });
 }
--- json/process.py
+++ json/process.py
@@ -1,7 +1,7 @@
 import codecs
 import json
 
-from urllib import unquote, quote
+from urllib import unquote
 
 from bs4 import BeautifulSoup
 
@@ -22,24 +22,28 @@ def write_json(path, data):
 
 
 
-html_file = 'kepviselo_list.html'
+html_file = 'kepviselok.html'
+
 soup = get_soup(html_file)
 table = soup.find('table')
 links = table.find_all('a', href=True)
 
 pdf_url_template = 'http://www.parlament.hu/egy-kepviselo-adatai?p_p_id=pairproxy_WAR_pairproxyportlet_INSTANCE_9xd2Wc9jP4z8&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_cacheability=cacheLevelPage&p_p_col_id=column-1&p_p_col_count=1&_pairproxy_WAR_pairproxyportlet_INSTANCE_9xd2Wc9jP4z8_pairAction=%2Finternet%2Fcplsql%2Fogy_vagyonpub.vagyon_kiir_egys%3FP_FNEV%3D%2F2014%2F{}_j0141231k.pdf%26p_cont%3Dapplication%2Fpdf'
+person_url_template = 'http://www.parlament.hu/aktiv-kepviseloi-nevsor?p_auth=wpyC4gC6&p_p_id=pairproxy_WAR_pairproxyportlet_INSTANCE_9xd2Wc9jP4z8&p_p_lifecycle=1&p_p_state=normal&p_p_mode=view&p_p_col_id=column-1&p_p_col_count=1&_pairproxy_WAR_pairproxyportlet_INSTANCE_9xd2Wc9jP4z8_pairAction=%2Finternet%2Fcplsql%2Fogy_kpv.kepv_adat%3Fp_azon%3D{}%26p_stilus%3D%26p_head%3D'
 
 data = {}
 
 for l in links:
     name = l.text
     href = unquote(l.attrs['href'])
-    id = href.split('p_azon=')[1].split('&p_ckl')[0]
+    id = href.split('p_azon=')[1].split('&p_stilus')[0]
     pdf_url = pdf_url_template.format(id)
+    person_url = person_url_template.format(id)
 
     data[id] = {
         'name': name,
-        'pdf_url': pdf_url
+        'pdf_url': pdf_url,
+        'person_url': person_url
     }
 
 write_json('data.json', data)
