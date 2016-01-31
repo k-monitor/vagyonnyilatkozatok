5e8b90b32b59c40c4c65a818d776062d2ded6c24
ejulcsi <ejulcsi@gmail.com>
2016-02-01 00:04:05 +0100

    new data

--- json/process.py
+++ json/process.py
@@ -28,8 +28,10 @@ soup = get_soup(html_file)
 table = soup.find('table')
 links = table.find_all('a', href=True)
 
-pdf_url_template = 'http://www.parlament.hu/egy-kepviselo-adatai?p_p_id=pairproxy_WAR_pairproxyportlet_INSTANCE_9xd2Wc9jP4z8&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_cacheability=cacheLevelPage&p_p_col_id=column-1&p_p_col_count=1&_pairproxy_WAR_pairproxyportlet_INSTANCE_9xd2Wc9jP4z8_pairAction=%2Finternet%2Fcplsql%2Fogy_vagyonpub.vagyon_kiir_egys%3FP_FNEV%3D%2F2014%2F{}_j0141231k.pdf%26p_cont%3Dapplication%2Fpdf'
+# pdf_url_template = 'http://www.parlament.hu/egy-kepviselo-adatai?p_p_id=pairproxy_WAR_pairproxyportlet_INSTANCE_9xd2Wc9jP4z8&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_cacheability=cacheLevelPage&p_p_col_id=column-1&p_p_col_count=1&_pairproxy_WAR_pairproxyportlet_INSTANCE_9xd2Wc9jP4z8_pairAction=%2Finternet%2Fcplsql%2Fogy_vagyonpub.vagyon_kiir_egys%3FP_FNEV%3D%2F2014%2F{}_j0141231k.pdf%26p_cont%3Dapplication%2Fpdf'
 person_url_template = 'http://www.parlament.hu/aktiv-kepviseloi-nevsor?p_auth=wpyC4gC6&p_p_id=pairproxy_WAR_pairproxyportlet_INSTANCE_9xd2Wc9jP4z8&p_p_lifecycle=1&p_p_state=normal&p_p_mode=view&p_p_col_id=column-1&p_p_col_count=1&_pairproxy_WAR_pairproxyportlet_INSTANCE_9xd2Wc9jP4z8_pairAction=%2Finternet%2Fcplsql%2Fogy_kpv.kepv_adat%3Fp_azon%3D{}%26p_stilus%3D%26p_head%3D'
+pdf_url_template = 'http://www.parlament.hu/aktiv-kepviseloi-nevsor?p_p_id=pairproxy_WAR_pairproxyportlet_INSTANCE_9xd2Wc9jP4z8&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_cacheability=cacheLevelPage&p_p_col_id=column-1&p_p_col_count=1&_pairproxy_WAR_pairproxyportlet_INSTANCE_9xd2Wc9jP4z8_pairAction=%2Finternet%2Fcplsql%2Fogy_vagyonpub.vagyon_kiir_egys%3FP_FNEV%3D%2F2015%2F{}_j0151231k.pdf%26p_cont%3Dapplication%2Fpdf'
+
 
 data = {}
 
